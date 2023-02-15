682edeb6fb19c683563dd0055ef3a7964d1ad15e
chutchai.kp <chutchai.kp@gmail.com>
2023-02-15 07:59:44 +0700

    commit

---
+++ ui/src/pages/about/About.jsx
@@ -0,0 +1,15 @@
+import './about.scss';
+const About = () => {
+  return (
+    <div className="about">
+      <h1> About </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, maxime
+        minima obcaecati voluptatibus laborum alias exercitationem? Ducimus eos
+        a repudiandae cupiditate dicta amet qui sed voluptas exercitationem
+        repellat, delectus dolorem.
+      </p>
+    </div>
+  );
+};
+export default About;
